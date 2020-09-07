c1e2ff307a247385c7fbf34d2ddb1ea8ccd97c06
Dan Cochran <dan.s.cochran@gmail.com>
2020-09-07 15:34:26 -0400

    working generator with BOM and properties

--- src/main/java/com/dan/ese/svcgen/SvcgenApplication.java
+++ src/main/java/com/dan/ese/svcgen/SvcgenApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SvcgenApplication {
+public class SvcgenApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(SvcgenApplication.class, args);
 	}
 
